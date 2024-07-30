8e21ac1208b8e568b8beea8fca0cd8d33df0968e
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2024-07-31 00:09:26 +0300

    added header

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,3 +1,25 @@
+// Header 
+
+const burger = document.querySelector('.burger');
+const linkClose = document.querySelectorAll('.link-close');
+const overflow = document.querySelector('.overflow');
+
+burger?.addEventListener('click', function () {
+  document.body.classList.toggle('body_lock');
+  document.body.classList.toggle('active');
+});
+
+overflow?.addEventListener('click', function () {
+  document.body.classList.toggle('body_lock');
+  document.body.classList.toggle('active');
+});
+
+for (let i = 0; i < linkClose.length; ++i) {
+  linkClose[i].addEventListener('click', function () {
+  document.body.classList.remove('body_lock');
+  document.body.classList.remove('active');
+  });
+}
 
 // Scroll anim
 
